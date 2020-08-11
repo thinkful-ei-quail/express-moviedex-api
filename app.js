e6673da9dfb879d1ae078f954bdfe216b69ae01c
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-08-11 17:00:26 -0500

    help

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const store = require('./store');
-const cores = require('cores');
+//const cors = require('cors');
 // let response = [...store];
 const app = express();
 // console.log(response)
@@ -17,28 +17,24 @@ app.get('/movie', (req, res) => {
 
   if (req.query.genre) {
     response = response.filter(movie => {
-      console.log(movie, "test movie")
+      //console.log(movie, 'test movie');
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase());
-      console.log("test", response)
+      //console.log('test', response);
     });
+    //return response;
   }
 
   if (req.query.country) {
     response = response.filter(movie => {
-      console.log(movie, "test movie")
+      //console.log(movie, 'test movie');
       movie.country.toLowerCase().includes(req.query.country.toLowerCase());
-      console.log("test", response)
+      //console.log('test', response);
     });
+    
   }
-  res.json(response)
-  
-
-
-  console.log(response)
-
-
+  res.json(response);
 });
 
-app.listen(9080, () => {
+app.listen(9000, () => {
   console.log('app deployed on port 8000!');
 });
