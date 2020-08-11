150426ee6928e299727002b058c80a15d63a7379
Nick Snyder <N_Snyder119@hotmail.com>
2020-08-11 14:27:41 -0700

    started filtering

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
-const app= express();
-
+const store = require('./store');
+const cores = require('cores');
+// let response = [...store];
+const app = express();
+// console.log(response)
 app.use(morgan('dev'));
 
-app.get('/movie', (req,res)=>{
-  const {genre, country, score} = req.query;
+
+
+
+// console.log(response.genre)
+app.get('/movie', (req, res) => {
+  let response = store;
+  // const { genre, country, score } = req.query;
+
+  if (req.query.genre) {
+    response = response.filter(movie => {
+      console.log(movie, "test movie")
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase());
+      console.log("test", response)
+    });
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie => {
+      console.log(movie, "test movie")
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase());
+      console.log("test", response)
+    });
+  }
+  res.json(response)
   
-  res.status(200).send('hello');
+
+
+  console.log(response)
+
+
 });
 
-app.listen(8000,() =>{
+app.listen(9080, () => {
   console.log('app deployed on port 8000!');
 });
