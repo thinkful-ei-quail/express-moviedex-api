c37b9e2c4899278ae1c535ded6f1ae746c2ce601
Nick Snyder <N_Snyder119@hotmail.com>
2020-08-11 16:47:35 -0700

    fixed filter

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const store = require('./store');
-const cores = require('cores');
+const cors = require('cors');
 // let response = [...store];
 const app = express();
 // console.log(response)
@@ -14,27 +14,28 @@ app.use(morgan('dev'));
 app.get('/movie', (req, res) => {
   let response = store;
   // const { genre, country, score } = req.query;
-
+console.log(req.query)
   if (req.query.genre) {
-    response = response.filter(movie => {
-      console.log(movie, "test movie")
-      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase());
-      console.log("test", response)
-    });
+    response = response.filter(movie => 
+      // console.log(movie, "test movie")
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+      // console.log("test", response)
+    );
+
   }
 
   if (req.query.country) {
-    response = response.filter(movie => {
-      console.log(movie, "test movie")
-      movie.country.toLowerCase().includes(req.query.country.toLowerCase());
-      console.log("test", response)
-    });
+    response = response.filter(movie => 
+      // console.log(movie, "test movie")
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+      // console.log("test", response)
+    );
   }
   res.json(response)
   
 
 
-  console.log(response)
+  // console.log(response)
 
 
 });
